bcc12ea2941da26dc9f324d0250fe5b587afce6b
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-02 20:12:25 +0200

    make sign in with email instead name

--- my_auth/models.py
+++ my_auth/models.py
@@ -4,4 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    bio = models.TextField(null=True)
+    bio = models.TextField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
