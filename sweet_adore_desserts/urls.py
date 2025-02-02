e43adf3461942b5806cb1b1ff59e9afe566f2d3b
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-02 19:47:51 +0200

    Install pillow. Add media url, media root settings and urls files.

--- sweet_adore_desserts/urls.py
+++ sweet_adore_desserts/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,4 +27,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('tinymce/', include('tinymce.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
