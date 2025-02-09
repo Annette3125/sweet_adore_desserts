8bc737a8d29e9a470bb9268900a0a4e2a876f27d
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-09 02:57:06 +0200

    make migrations (for login with email)

--- desserts/admin.py
+++ desserts/admin.py
@@ -1,50 +1,32 @@
 from django.contrib import admin
-from django.forms import ModelForm
 
-from desserts.models import (Cake, CakeByDesign, CakeByFlavour, CakeByOccasion,
-                             CakePop, Cocktail, Cookie, Order)
+from desserts.models import (Option, Category, Product,
+                             OrderLine, Order, Cocktail)
 
-
-class CakeAdmin(admin.ModelAdmin):
-    list_display = ["cake_by_occasion", "cake_by_flavour", "cake_by_design"]
-    list_filter = ["cake_by_occasion", "cake_by_flavour", "cake_by_design"]
-
-
-class CakeByOccasionAdmin(admin.ModelAdmin):
-    list_display = ["by_occasion", "description"]
-
-
-class CakeByDesignAdmin(admin.ModelAdmin):
-    list_display = ["by_design", "description"]
-
-
-class CakeByFlavourAdmin(admin.ModelAdmin):
-    list_display = ["by_flavour", "description"]
-
-
-class CookieAdmin(admin.ModelAdmin):
-    list_display = ["cookie", "price", "quantity", "description"]
-    list_editable = ["price"]
-
-
-class CakePopAdmin(admin.ModelAdmin):
-    list_display = ["cake_pops", "price", "quantity", "description"]
-    list_editable = ["price"]
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    extra = 0
 
 
 class OrderAdmin(admin.ModelAdmin):
-    model = Order
-    list_display = [
-        "cake",
-        "cookie",
-        "cake_pops",
-        "date",
-        "total_price",
-        "user",
-        "deadline",
-    ]
-    search_fields = ["cake", "cookie", "cake_pops"]
+    readonly_fields = ["id"]
+    inlines = [OrderLineInline]
+    list_display = ["order_date", "total_price", "user", "deadline", "product"]
+    list_editable = ["total_price", "deadline"]
 
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["category", "name", "description", "price"]
+    list_filter = ["category", "name", "price"]
+    list_editable = ["name", "description", "price"]
+    search_fields = ["category", "name"]
 
 class CocktailAdmin(admin.ModelAdmin):
     model = Cocktail
@@ -52,11 +34,10 @@ class CocktailAdmin(admin.ModelAdmin):
     list_editable = ["description", "recipe"]
 
 
-admin.site.register(Cake, CakeAdmin)
-admin.site.register(CakeByOccasion, CakeByOccasionAdmin)
-admin.site.register(CakeByDesign, CakeByDesignAdmin)
-admin.site.register(CakeByFlavour, CakeByFlavourAdmin)
-admin.site.register(Cookie, CookieAdmin)
-admin.site.register(CakePop, CakePopAdmin)
+admin.site.register(Option, OptionAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(OrderLine)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Cocktail, CocktailAdmin)
+
