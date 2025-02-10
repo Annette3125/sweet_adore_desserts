c5d06022bddd8c61eb5153aadd0c58751477c3a8
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-11 01:52:49 +0200

    update index html and views by retrieving and counting objects

--- desserts/views.py
+++ desserts/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse
+from .forms import OrderLineForm
+from .models import Option, Product, Cocktail, Category
 
 
 def index(request):
-    context = {"cakes": cakes}
+
+    num_cakes = Option.objects.all().count
+    num_cookies = Product.objects.filter(category__name="Cookies").count()
+    num_cake_pops = Product.objects.filter(category__name="Cake_Pops").count()
+    num_cocktails_recipes = Cocktail.objects.all().count
+    context = {"num_cakes": num_cakes,
+               "num_cookies": num_cookies,
+               "num_cake_pops": num_cake_pops,
+               "num_cocktails_recipes": num_cocktails_recipes,
+               }
+
     return render(request, "desserts/index.html", context=context)
 
 
