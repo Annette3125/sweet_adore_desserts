a80620f573c86b772c4d1cb4aa4f6f4ea002bf10
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-11 01:40:39 +0200

    create model forms

--- desserts/forms.py
+++ desserts/forms.py
@@ -1,8 +1,29 @@
 from django.forms import ModelForm
-from models import Cake
+from .models import Option, Category, Product, Order, OrderLine, Cocktail
 
 
-class CakeForm(ModelForm):
+class OptionForm(ModelForm):
     class Meta:
-        model = Cake
+        model = Option
+        fields = "__all__"
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+class OrderLineForm(ModelForm):
+    class Meta:
+        model = OrderLine
         fields = "__all__"
