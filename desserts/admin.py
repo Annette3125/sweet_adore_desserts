307ab85d3e56c1f9b246bed6ed174d8c65350978
Aneta Skrodenienė <anetuks@gmail.com>
2025-02-17 07:28:28 +0200

    update admin - delete total price

--- desserts/admin.py
+++ desserts/admin.py
@@ -11,8 +11,8 @@ class OrderLineInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ["id"]
     inlines = [OrderLineInline]
-    list_display = ["order_date", "total_price", "user", "deadline", "status"]
-    list_editable = ["total_price", "deadline", "status"]
+    list_display = ["order_date","total_price", "user", "deadline", "status"]
+    list_editable = ["deadline", "status"]
 
 class OptionAdmin(admin.ModelAdmin):
     list_display = ["name"]
