d3609628424747aebf22d5ed39b1c5075ee66093
Aneta Skrodenienė <anetuks@gmail.com>
2025-03-05 09:33:36 +0200

    write test for OrderLine

--- desserts/tests.py
+++ desserts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import OrderLine, Product
 
-# Create your tests here.
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sweet_adore_desserts.settings")
+django.setup()
+
+class OrderLineTestCase(TestCase):
+    def test_order_line_price(self):
+        product = Product.objects.create(name="Cake", price=10.0)
+        order_line = OrderLine.objects.create(product=product, quantity=2)
+        self.assertEqual(order_line.price, 20.0)
