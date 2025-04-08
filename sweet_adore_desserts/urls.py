efefec43792c9378256c06a231bba4bf7ae05051
Aneta Skrodenienė <anetuks@gmail.com>
2025-04-09 01:49:42 +0300

    apply styling and one profile picture

--- sweet_adore_desserts/urls.py
+++ sweet_adore_desserts/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path("register/", include("my_auth.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('tinymce/', include('tinymce.urls')),
+    path("tinymce/", include("tinymce.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
